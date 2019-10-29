4bc11437d4bba32d406b28f6833dc5eef3787c34
Mikael Remes <pojankolli@gmail.com>
2019-10-29 10:56:59 +0200

    Added some small things

---
+++ app/src/main/java/com/example/gamificationapp/ExcersizeActivity.java
@@ -0,0 +1,14 @@
+package com.example.gamificationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExcersizeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_excersize);
+    }
+}
